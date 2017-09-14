b805da7ef2c252ebf03acea9e45791c981bab10b
1103-04 <1103-04@1103-04-PC>
2017-09-14 12:50:50 +0900

    작업중

---
+++ chap06_TOTAL/src/main/java/org/itbank/app/controller/MemberController.java
@@ -0,0 +1,19 @@
+package org.itbank.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/my/")
+public class MemberController {
+
+	@GetMapping("/info/")
+	public String infoHandle() { 
+		return "t_expr"; 
+		
+		
+	}
+	
+
+}
